50a3984aa4299008898cdb593c9e20276e7d5f1f
DaoChaShao <27882272@qq.com>
2025-10-22 01:36:22 +0800

    feat: add 01_jieba_04_id2word.py for word-to-id mapping and stopwords filtering

---
+++ background/01_jieba_04_id2word.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     :   2025/10/21 23:09
+# @Author   :   Shawn
+# @Version  :   Version 0.1.0
+# @File     :   01_jieba_04_id2word.py
+# @Desc     :   
+
+from utils.config import EXAMPLES_CHINESE_PATH, EXAMPLES_CHINESE_STOPWORDS_PATH
+from utils.helper import read_file
+from utils.JB import cut_accuracy
+from utils.nlp import regular_chinese
+
+
+def main() -> None:
+    """ Main Function """
+    content: str = read_file(EXAMPLES_CHINESE_PATH)
+
+    # Read stopwords from file without repeats
+    stopwords: set[str] = set(read_file(EXAMPLES_CHINESE_STOPWORDS_PATH).splitlines())
+
+    words = cut_accuracy(content)
+    # Remove stopwords from the cut words
+    words_stop: list[str] = [word for word in words if word not in stopwords]
+
+    # Use regex to filter only Chinese characters
+    vocabs = regular_chinese(words_stop)
+
+    # Avoid repeats by converting to set and back to list, which is call 'id2word' technique
+    id2word = list(set(vocabs))
+    print(id2word)
+
+    # word to id mapping
+    word2id = {word: index for index, word in enumerate(id2word)}
+    print(word2id)
+
+
+if __name__ == "__main__":
+    main()
