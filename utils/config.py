626e9d851c8413ed9edea503e62309b3e709e619
DaoChaShao <27882272@qq.com>
2025-10-21 21:43:06 +0800

    feat: add config.py with initial metadata and versioning information

---
+++ utils/config.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     :   2025/10/21 21:39
+# @Author   :   Shawn
+# @Version  :   Version 0.1.0
+# @File     :   config.py
+# @Desc     :   
+
+
