aa1a3e765a8b4ca2ffe7723d43eaea76f9716547
DaoChaShao <27882272@qq.com>
2025-10-22 01:36:35 +0800

    feat: add 02_thu_01_cut.py for Chinese text cutting and segmentation functionalities

---
+++ background/02_thu_01_cut.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     :   2025/10/21 23:52
+# @Author   :   Shawn
+# @Version  :   Version 0.1.0
+# @File     :   02_thu_01_cut.py
+# @Desc     :   
+
+from utils.config import EXAMPLES_CHINESE_PATH
+from utils.helper import read_file
+from utils.THU import cut_only, cut_segmentation
+
+
+def main() -> None:
+    """ Main Function """
+    content = read_file(EXAMPLES_CHINESE_PATH)
+
+    words = cut_only(content)
+    vocabs = cut_segmentation(content)
+
+    print(words)
+    print(vocabs)
+
+
+if __name__ == "__main__":
+    main()
