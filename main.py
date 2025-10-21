5d0ee29ca9c9ba1206370f05793d2ed36a3b2083
DaoChaShao <27882272@qq.com>
2025-10-22 01:37:32 +0800

    feat: implement main function to read content from examples Chinese path

--- main.py
+++ main.py
@@ -4,11 +4,15 @@
 # @Author   :   Shawn
 # @Version  :   Version 0.1.0
 # @File     :   main.py
-# @Desc     :   
+# @Desc     :
+
+from utils.config import EXAMPLES_CHINESE_PATH
+from utils.helper import read_file
+
 
 def main() -> None:
     """ Main Function """
-    pass
+    content = read_file(EXAMPLES_CHINESE_PATH)
 
 
 if __name__ == "__main__":
