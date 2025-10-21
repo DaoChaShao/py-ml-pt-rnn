2a8758e19327f178038d0e37f62225dee02f39db
DaoChaShao <27882272@qq.com>
2025-10-22 01:36:11 +0800

    feat: add 01_jieba_02_stopwords.py for stopwords removal in Chinese text segmentation

---
+++ background/01_jieba_02_stopwords.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     :   2025/10/21 22:49
+# @Author   :   Shawn
+# @Version  :   Version 0.1.0
+# @File     :   01_jieba_02_stopwords.py
+# @Desc     :   
+
+from utils.config import EXAMPLES_CHINESE_PATH, EXAMPLES_CHINESE_STOPWORDS_PATH
+from utils.helper import read_file
+from utils.JB import cut_accuracy
+
+
+def main() -> None:
+    """ Main Function """
+    content: str = read_file(EXAMPLES_CHINESE_PATH)
+
+    # Read stopwords from file without repeats
+    stopwords: set[str] = set(read_file(EXAMPLES_CHINESE_STOPWORDS_PATH).splitlines())
+    print(stopwords)
+
+    words = cut_accuracy(content)
+    # Remove stopwords from the cut words
+    vocabs: list[str] = [word for word in words if word not in stopwords]
+    print(words)
+    print(vocabs)
+
+
+if __name__ == "__main__":
+    main()
