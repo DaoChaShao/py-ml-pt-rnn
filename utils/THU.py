55e4d8ff7c32fb91f53133c9f72ebcf1960327d8
DaoChaShao <27882272@qq.com>
2025-10-22 01:37:47 +0800

    feat: add THU.py for text segmentation using THULAC

---
+++ utils/THU.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     :   2025/10/22 01:07
+# @Author   :   Shawn
+# @Version  :   Version 0.1.0
+# @File     :   THU.py
+# @Desc     :   
+
+from thulac import thulac
+
+from utils.decorator import timer
+
+
+@timer
+def cut_segmentation(text: str) -> tuple[list[tuple[str, str]], list[str]]:
+    """ Cut text using THULAC
+    :param text: text to cut
+    :return: list of tuples of cut words and their POS tags
+    """
+    # Initialize THULAC with segmentation only
+    thu = thulac(seg_only=False)
+    words_tag: list[tuple[str, str]] = thu.cut(text)
+    words: list[str] = [word for word, tag in words_tag]
+
+    print(f"The text has been cut into {len(words)} words using THULAC.")
+
+    return words_tag, words
+
+
+@timer
+def cut_only(text: str) -> list[str]:
+    """ Cut text using THULAC without POS tags
+    :param text: text to cut
+    :return: list of cut words
+    """
+    # Initialize THULAC with segmentation only
+    thu = thulac(seg_only=True)
+    words: list[str] = thu.cut(text)
+
+    print(f"The text has been cut into {len(words)} words using THULAC (without POS tags).")
+
+    return words
