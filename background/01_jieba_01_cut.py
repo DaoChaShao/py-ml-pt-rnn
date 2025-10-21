4c607d603bbc9d5101652eb009079849a5f0068d
DaoChaShao <27882272@qq.com>
2025-10-22 01:36:05 +0800

    feat: add 01_jieba_01_cut.py for Chinese text segmentation functionalities

---
+++ background/01_jieba_01_cut.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     :   2025/10/21 21:50
+# @Author   :   Shawn
+# @Version  :   Version 0.1.0
+# @File     :   01_jieba_01_cut.py
+# @Desc     :   
+
+from utils.config import EXAMPLES_CHINESE_PATH
+from utils.helper import read_file
+from utils.JB import cut_accuracy, cut_full, cut_search
+
+
+def main() -> None:
+    """ Main Function """
+    content: str = read_file(EXAMPLES_CHINESE_PATH)
+
+    acc = cut_accuracy(content)
+    print(acc)
+    print()
+
+    full = cut_full(content)
+    print(full)
+    print()
+
+    search = cut_search(content)
+    print(search)
+    print()
+
+
+if __name__ == "__main__":
+    main()
