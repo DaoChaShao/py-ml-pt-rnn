553c8329a42b77a40f081f7438f3afa6a1750874
DaoChaShao <27882272@qq.com>
2025-10-22 01:36:42 +0800

    feat: add 02_thu_02_stopwords.py for stopwords filtering in Chinese text segmentation

---
+++ background/02_thu_02_stopwords.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     :   2025/10/22 00:44
+# @Author   :   Shawn
+# @Version  :   Version 0.1.0
+# @File     :   02_thu_02_stopwords.py
+# @Desc     :
+
+from utils.config import EXAMPLES_CHINESE_PATH, EXAMPLES_CHINESE_STOPWORDS_PATH
+from utils.helper import read_file
+from utils.THU import cut_only
+
+
+def main() -> None:
+    """ Main Function """
+    content = read_file(EXAMPLES_CHINESE_PATH)
+    words = cut_only(content)
+
+    stopwords: set[str] = set(read_file(EXAMPLES_CHINESE_STOPWORDS_PATH).splitlines())
+    print(stopwords)
+    vocabs = [word for subwords in words for word in subwords if word not in stopwords]
+
+    print(words)
+    print(vocabs)
+
+
+if __name__ == "__main__":
+    main()
