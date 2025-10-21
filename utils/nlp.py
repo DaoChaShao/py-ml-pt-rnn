87aa84e0914fb7db9d1c42beacd1710678d58402
DaoChaShao <27882272@qq.com>
2025-10-22 01:37:37 +0800

    feat: add regular_chinese function to filter and retain only Chinese characters from a list

--- utils/nlp.py
+++ utils/nlp.py
@@ -4,6 +4,24 @@
 # @Author   :   Shawn
 # @Version  :   Version 0.1.0
 # @File     :   nlp.py
-# @Desc     :   
+# @Desc     :
 
 
+from re import compile
+
+from utils.decorator import timer
+
+
+@timer
+def regular_chinese(words: list[str]) -> list[str]:
+    """ Retain only Chinese characters in the list of words
+    :param words: list of words to process
+    :return: list of words containing only Chinese characters
+    """
+    pattern = compile(r"[\u4e00-\u9fa5]+")
+
+    chinese: list[str] = [word for word in words if pattern.fullmatch(word)]
+
+    print(f"Retained {len(chinese)} Chinese words from the original {len(words)} words.")
+
+    return chinese
