e8e84ab77e53ad10c1ff714705fdb54e9ff6feb8
DaoChaShao <27882272@qq.com>
2025-10-22 15:57:16 +0800

    refactor: update file path configuration in main function

--- background/01_jieba_01_cut.py
+++ background/01_jieba_01_cut.py
@@ -6,14 +6,14 @@
 # @File     :   01_jieba_01_cut.py
 # @Desc     :   
 
-from utils.config import EXAMPLES_CHINESE_PATH
+from utils.config import CONFIG
 from utils.helper import read_file
 from utils.JB import cut_accuracy, cut_full, cut_search
 
 
 def main() -> None:
     """ Main Function """
-    content: str = read_file(EXAMPLES_CHINESE_PATH)
+    content: str = read_file(CONFIG.FILEPATHS.EXAMPLE_PAPER_ZH)
 
     acc = cut_accuracy(content)
     print(acc)
