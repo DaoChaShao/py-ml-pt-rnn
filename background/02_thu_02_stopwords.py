0835be1a42c7992d5ce0243fae935b8baf95b912
DaoChaShao <27882272@qq.com>
2025-10-22 15:57:47 +0800

    refactor: update file paths in 02_thu_02_stopwords.py to use centralized configuration

--- background/02_thu_02_stopwords.py
+++ background/02_thu_02_stopwords.py
@@ -6,17 +6,17 @@
 # @File     :   02_thu_02_stopwords.py
 # @Desc     :
 
-from utils.config import EXAMPLES_CHINESE_PATH, EXAMPLES_CHINESE_STOPWORDS_PATH
+from utils.config import CONFIG
 from utils.helper import read_file
 from utils.THU import cut_only
 
 
 def main() -> None:
     """ Main Function """
-    content = read_file(EXAMPLES_CHINESE_PATH)
+    content = read_file(CONFIG.FILEPATHS.EXAMPLE_PAPER_ZH)
     words = cut_only(content)
 
-    stopwords: set[str] = set(read_file(EXAMPLES_CHINESE_STOPWORDS_PATH).splitlines())
+    stopwords: set[str] = set(read_file(CONFIG.FILEPATHS.EXAMPLE_STOPWORDS_ZH).splitlines())
     print(stopwords)
     vocabs = [word for subwords in words for word in subwords if word not in stopwords]
 
