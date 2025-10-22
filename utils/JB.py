d81c3687b862b087948b8afb28caea7a8c3de9d3
DaoChaShao <27882272@qq.com>
2025-10-22 13:30:32 +0800

    feat: refactor text processing functions to use 'content' parameter and add POS tagging functionality

--- utils/JB.py
+++ utils/JB.py
@@ -6,8 +6,11 @@
 # @File     :   JB.py
 # @Desc     :
 
-from jieba import lcut, lcut_for_search, analyse
+from jieba import (lcut, lcut_for_search,
+                   analyse,
+                   posseg)
 from pandas import DataFrame
+from re import compile
 
 from utils.decorator import timer
 
@@ -26,12 +29,12 @@ def cut_accuracy(text: str) -> list[str]:
 
 
 @timer
-def cut_full(text: str) -> list[str]:
+def cut_full(content: str) -> list[str]:
     """ Cut text for jieba full mode result
-    :param text: text to cut
+    :param content: text to cut
     :return: list of cut words
     """
-    words: list[str] = lcut(text, cut_all=True)
+    words: list[str] = lcut(content, cut_all=True)
 
     print(f"The text has been cut into {len(words)} words.")
 
@@ -39,12 +42,12 @@ def cut_full(text: str) -> list[str]:
 
 
 @timer
-def cut_search(text: str) -> list[str]:
+def cut_search(content: str) -> list[str]:
     """ Cut text for jieba search engine mode result
-    :param text: text to cut
+    :param content: text to cut
     :return: list of cut words
     """
-    words: list[str] = lcut_for_search(text)
+    words: list[str] = lcut_for_search(content)
 
     print(f"The text has been cut into {len(words)} words.")
 
@@ -52,50 +55,70 @@ def cut_search(text: str) -> list[str]:
 
 
 @timer
-def extract_tfidf_weights(text: str, top_k: int = 10, pos: bool = False) -> tuple[list[tuple[str, float]], DataFrame]:
+def cut_pos(content: str, restriction: str, top_k: int = 10):
+    """ Cut text and get part of speech"""
+    dictionary: dict[str, str] = {}
+
+    words = posseg.lcut(content, use_paddle=True)
+    # print(words)
+
+    pattern = compile(restriction)
+    for word, pos in words:
+        if pattern.match(word) and len(word.strip()) > 1:
+            dictionary[word] = pos
+
+    cols = ["word", "pos"]
+    df = DataFrame(dictionary.items(), columns=cols)[:top_k]
+    print(f"Cut POS Results:\n{df}")
+
+    return dictionary, df
+
+
+@timer
+def extract_tfidf_weights(content: str, top_k: int = 10, pos: bool = False) -> tuple[list, DataFrame]:
     """ Extract TF-IDF weights from text
-    :param text: the text to cut
+    :param content: the text to cut
     :param top_k: number of top keywords to extract
     :param pos: whether to filter by part of speech
     :return: list of tuples containing words and their weights, and a DataFrame of the same
     """
     mask: tuple = ("v", "vn", "a", "d") if pos else ()
     tags = analyse.extract_tags(
-        text,
+        content,
         topK=top_k,
         withWeight=True,
         allowPOS=mask,
     )
 
-    cols = ["word", "weight"]
-    dataframe = DataFrame(data=tags, columns=cols)
+    cols: list[str] = ["word", "weight"]
+    df: DataFrame = DataFrame(data=tags, columns=cols)
 
-    print(f"TF-IDF words weights:\n{dataframe}")
+    print(f"TF-IDF words weights:\n{df}")
     print(f"Extracted {len(tags)} tags using TF-IDF.")
 
-    return tags, dataframe
+    return tags, df
 
 
 @timer
-def extract_textrank_words(text: str, top_k: int = 10, pos: bool = False) -> tuple[list[tuple[str, float]], DataFrame]:
+def extract_textrank_words(content: str, top_k: int = 10, pos: bool = False) -> tuple[list, DataFrame]:
     """ Extract TextRank words from text
-    :param text: the text to cut
+    :param content: the text to cut
     :param top_k: number of top keywords to extract
     :param pos: whether to filter by part of speech
     :return: list of tuples containing words and their weights, and a DataFrame of the same
     """
     mask: tuple = ("v", "vn", "a", "d") if pos else ()
     words = analyse.textrank(
-        text,
+        content,
         topK=top_k,
         withWeight=True,
         allowPOS=mask,
     )
 
-    cols = ["word", "weight"]
-    dataframe = DataFrame(data=words, columns=cols)
+    cols: list[str] = ["word", "weight"]
+    df: DataFrame = DataFrame(data=words, columns=cols)
 
-    print(f"TexTank Words Weights:\n{dataframe}")
+    print(f"TexTank Words Weights:\n{df}")
     print(f"Extracted {len(words)} words using TextRank.")
 
-    return words, dataframe
+    return words, df
