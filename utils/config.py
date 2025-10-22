a7616628c927778ff4bedf690290d9c3edcb7a48
DaoChaShao <27882272@qq.com>
2025-10-23 00:48:06 +0800

    refactor: update configuration in config.py to improve organization and add model parameters

--- utils/config.py
+++ utils/config.py
@@ -8,6 +8,7 @@
 
 from dataclasses import dataclass, field
 from pathlib import Path
+from torch import cuda
 from types import SimpleNamespace
 
 # Set base directory
@@ -17,11 +18,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 CONFIGRATION: SimpleNamespace = SimpleNamespace(
     # File paths
     PATHS=SimpleNamespace(
-        MODEL_SAVE=BASE_DIR / "models/model.pth",
+        MODEL_SAVE=BASE_DIR / "models/model_seq.pth",
         EXAMPLE_PAPER_ZH=BASE_DIR / "data/Chinese.txt",
         EXAMPLE_STOPWORDS_ZH=BASE_DIR / "data/stopwords4background.txt",
-        DATASET_TRAIN=BASE_DIR / "data/",
-        DATASET_SET=BASE_DIR / "data/",
+        POEMS_JSON=BASE_DIR / "data/poems.json",
+        POEMS=BASE_DIR / "data/poems.txt",
     ),
     # Preprocessing settings
     PREPROCESSOR=SimpleNamespace(
@@ -43,38 +44,44 @@ CONFIGRATION: SimpleNamespace = SimpleNamespace(
 
 
 @dataclass
-class Paths:
-    MODEL_SAVE: Path = BASE_DIR / "models/model.pth"
+class FilePaths:
+    MODEL_SAVE: Path = BASE_DIR / "models/model_seq.pth"
     EXAMPLE_PAPER_ZH: Path = BASE_DIR / "data/Chinese.txt"
     EXAMPLE_STOPWORDS_ZH: Path = BASE_DIR / "data/stopwords4background.txt"
     POEMS_JSON: Path = BASE_DIR / "data/poems.json"
     POEMS: Path = BASE_DIR / "data/poems.txt"
-    DATASET_TRAIN: Path = BASE_DIR / "data/"
-    DATASET_TEST: Path = BASE_DIR / "data/"
 
 
 @dataclass
-class Preprocessor:
+class DataPreprocessor:
     RANDOM_STATE: int = 27
     VALID_SIZE: float = 0.2
     IS_SHUFFLE: bool = True
     PCA_VARIANCE_THRESHOLD: float = 0.95
+    BATCH_SIZE: int = 32
+
+
+@dataclass
+class ModelParameters:
+    SEQUENTIAL_LENGTH: int = 12
+    EMBEDDING_DIM: int = 256
+    HIDDEN_SIZE: int = 512
+    RNN_LAYERS: int = 2
 
 
 @dataclass
 class Hyperparameters:
-    BATCH_SIZE: int = 32
-    HIDDEN_UNITS: int = 128
-    ALPHA: float = 0.01
+    ALPHA: float = 1e-3
     ALPHA_REDUCTION: float = 0.3
     EPOCHS: int = 20
-    ACCELERATOR: str = "cpu"
+    ACCELERATOR: str = "cuda" if cuda.is_available() else "cpu"
 
 
 @dataclass
 class Configration:
-    FILEPATHS: Paths = field(default_factory=Paths)
-    PREPROCESSOR: Preprocessor = field(default_factory=Preprocessor)
+    FILEPATHS: FilePaths = field(default_factory=FilePaths)
+    PREPROCESSOR: DataPreprocessor = field(default_factory=DataPreprocessor)
+    PARAMETERS: ModelParameters = field(default_factory=ModelParameters)
     HYPERPARAMETERS: Hyperparameters = field(default_factory=Hyperparameters)
 
 
