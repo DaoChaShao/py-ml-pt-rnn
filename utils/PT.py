4f256c8121dfccabccb95b940ef930927591cd37
DaoChaShao <27882272@qq.com>
2025-10-23 00:48:32 +0800

    feat: add SequentialTorchDataset class for handling sequential features and labels

--- utils/PT.py
+++ utils/PT.py
@@ -9,7 +9,7 @@
 from numpy import ndarray, random as np_random
 from pandas import DataFrame, Series
 from random import seed as rnd_seed, getstate, setstate
-from torch import (cuda, backends, Tensor, tensor, float32, int64, long,
+from torch import (cuda, backends, Tensor, tensor, float32, int64,
                    manual_seed, get_rng_state, set_rng_state)
 
 from torch.utils.data import Dataset, DataLoader
@@ -70,6 +70,7 @@ class TorchRandomSeed:
         return f"{self._description!r} is set to randomness {self._seed}."
 
 
+@timer
 def check_device() -> None:
     """ Check Available Device (CPU, GPU, MPS)
     :return: dictionary of available devices
@@ -94,6 +95,7 @@ def check_device() -> None:
         print("Due to GPU or MPS unavailable, using CPU.")
 
 
+@timer
 def get_device(accelerator: str = "auto", cuda_mode: int = 0) -> str:
     """ Get the appropriate device based on the target device string
     :param accelerator: the target device string ("auto", "cuda", "mps", "cpu")
@@ -223,8 +225,8 @@ class GrayTensorReshaper:
         return f"GrayTensorReshape({self._image.shape})"
 
 
-class TorchDataset(Dataset):
-    """ A custom PyTorch Dataset class for handling features and labels """
+class LabelTorchDataset(Dataset):
+    """ A custom PyTorch Dataset class for handling label features and labels """
 
     def __init__(self, features: Any, labels: Any):
         """ Initialise the TorchDataset class
@@ -236,44 +238,36 @@ class TorchDataset(Dataset):
 
     @property
     def features(self) -> Tensor:
+        """ Return the feature tensor as a property """
         return self._features
 
     @property
     def labels(self) -> Tensor:
+        """ Return the label tensor as a property """
         return self._labels
 
     @staticmethod
     def _to_tensor(data: Union[DataFrame, Tensor, ndarray, list], is_label: bool = False) -> Tensor:
         """ Convert input data to a PyTorch tensor on the specified device
         :param data: the input data (DataFrame, ndarray, list, or Tensor)
+        :param is_label: whether the data is label data
         :return: the converted PyTorch tensor
         """
         if isinstance(data, (DataFrame, Series)):
-            if is_label:
-                out = tensor(data.values, dtype=int64)
-            else:
-                out = tensor(data.values, dtype=float32)
+            out = tensor(data.values, dtype=int64 if is_label else float32)
         elif isinstance(data, Tensor):
-            if is_label:
-                out = data.long()
-            else:
-                out = data.float()
+            out = data.long() if is_label else data.float()
         elif isinstance(data, (ndarray, list)):
-            if is_label:
-                out = tensor(data, dtype=int64)
-            else:
-                out = tensor(data, dtype=float32)
+            out = tensor(data, dtype=int64 if is_label else float32)
         elif isinstance(data, GrayTensorReshaper):
-            if is_label:
-                out = data().long()
-            else:
-                out = data().float()
+            out = data().long() if is_label else data().float()
         else:
             raise TypeError(f"Unsupported data type: {type(data)}")
 
         return out
 
     def __len__(self) -> int:
+        """ Return the total number of samples in the dataset """
         return len(self._features)
 
     def __getitem__(self, index: Union[int, slice]) -> Union[tuple[Tensor, Tensor], tuple[Tensor, Tensor]]:
@@ -288,7 +282,8 @@ class TorchDataset(Dataset):
             raise TypeError(f"Invalid index type: {type(index)}")
 
     def __repr__(self):
-        return f"TorchDataset(features={self._features.shape}, labels={self._labels.shape}, device=cpu)"
+        """ Return a string representation of the dataset """
+        return f"LabelTorchDataset(features={self._features.shape}, labels={self._labels.shape}, device=cpu)"
 
 
 class TorchDataLoader:
@@ -300,7 +295,7 @@ class TorchDataLoader:
         :param batch_size: the number of samples per batch
         :param is_shuffle: whether to shuffle the data at every epoch
         """
-        self._dataset: Union[Dataset, TorchDataset] = dataset
+        self._dataset: Union[Dataset, LabelTorchDataset] = dataset
         self._batches: int = batch_size
         self._is_shuffle: bool = is_shuffle
 
@@ -311,7 +306,7 @@ class TorchDataLoader:
         )
 
     @property
-    def dataset(self) -> Union[Dataset, TorchDataset]:
+    def dataset(self) -> Union[Dataset, LabelTorchDataset]:
         return self._dataset
 
     def __getitem__(self, index: int) -> tuple[Tensor, Tensor]:
@@ -330,3 +325,94 @@ class TorchDataLoader:
         return (f"TorchDataLoader(dataset={self._dataset}, "
                 f"batch_size={self._batches}, "
                 f"shuffle={self._is_shuffle})")
+
+
+class SequentialTorchDataset(Dataset):
+    """ A custom PyTorch Dataset class for handling sequential features and labels """
+
+    def __init__(self, sequences: list, sequence_length: int, pad_token: int) -> None:
+        """ Initialise the TorchDataset class for sequential data
+        :param sequences: the input sequences
+        :param sequence_length: the length of each sequence
+        :param pad_token: the padding token to use
+        """
+        self._sequences = sequences
+        self._length = sequence_length
+        self._pad = pad_token
+        self._features, self._labels = self._pad_seq_to_tensor()
+
+    def _pad_single_to_tensor(self) -> tuple[Tensor, Tensor]:
+        """ Convert input data to a PyTorch tensor via padding
+        :return: the converted PyTorch tensor
+        """
+        _features, _labels = [], []
+        for i in range(len(self._sequences) - 1):
+            if i < self._length - 1:
+                feature = [self._pad] * (self._length - i - 1) + self._sequences[0: i + 1]
+            else:
+                feature = self._sequences[i - self._length + 1: i + 1]
+            label = self._sequences[i + 1]
+            _features.append(feature)
+            _labels.append(label)
+
+        return tensor(_features), tensor(_labels)
+
+    def _pad_seq_to_tensor(self) -> tuple[Tensor, Tensor]:
+        """ Convert input data to a PyTorch tensor via sequence padding
+        :return: the converted PyTorch tensor
+        """
+        _features, _labels = [], []
+        for i in range(len(self._sequences) - 1):
+            if i < self._length - 1:
+                feature = [self._pad] * (self._length - i - 1) + self._sequences[0: i + 1]
+                label = [self._pad] * (self._length - i - 1) + self._sequences[1: i + 2]
+            else:
+                feature = self._sequences[i - self._length + 1: i + 1]
+                label = self._sequences[i - self._length + 2: i + 2]
+
+            _features.append(feature)
+            _labels.append(label)
+
+        return tensor(_features), tensor(_labels)
+
+    def _slide_to_tensor(self) -> tuple[Tensor, Tensor]:
+        """ Convert input data to a PyTorch tensor via sliding window
+        :return: the converted PyTorch tensor
+        """
+        _features, _labels = [], []
+        for i in range(len(self._sequences) - self._length):
+            feature = self._sequences[i: i + self._length]
+            label = self._sequences[i + self._length]
+            _features.append(feature)
+            _labels.append(label)
+
+        return tensor(_features), tensor(_labels)
+
+    @property
+    def features(self) -> Tensor:
+        """ Return the feature tensor as a property """
+        return self._features
+
+    @property
+    def labels(self) -> Tensor:
+        """ Return the label tensor as a property """
+        return self._labels
+
+    def __len__(self) -> int:
+        """ Return the total number of samples in the dataset """
+        return len(self._features)
+
+    def __getitem__(self, index: Union[int, slice]) -> tuple[Tensor, Tensor]:
+        """ Return a single (feature, label) pair or a batch via slice """
+        if isinstance(index, slice):
+            # Return a batch (for example dataset[:5])
+            return self._features[index], self._labels[index]
+        elif isinstance(index, int):
+            # Return a single sample
+            return self._features[index], self._labels[index]
+        else:
+            raise TypeError(f"Invalid index type: {type(index)}")
+
+    def __repr__(self):
+        """ Return a string representation of the dataset """
+        return f"SequentialTorchDataset(features={self._features.shape}, labels={self._labels.shape}, device=cpu)"
